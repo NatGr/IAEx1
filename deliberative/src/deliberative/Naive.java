1dd67422d32a6b1e218aba5174852f6d62eb928f
Nathan Greffe <N.Greffe@student.ulg.ac.be>
2018-10-14 13:57:03 +0200

    forgot naive

---
+++ deliberative/src/deliberative/Naive.java
@@ -0,0 +1,32 @@
+package deliberative;
+
+import logist.plan.Plan;
+import logist.task.Task;
+import logist.topology.Topology.City;
+
+public class Naive implements Algorithm{
+
+	@Override
+	public Plan plan(State initState) {
+		City current = initState.city;
+		Plan plan = new Plan(current);
+		
+		for (Task task : initState.availableTasks) {
+			// move: current city => pickup location
+			for (City city : current.pathTo(task.pickupCity))
+				plan.appendMove(city);
+
+			plan.appendPickup(task);
+
+			// move: pickup location => delivery location
+			for (City city : task.path())
+				plan.appendMove(city);
+
+			plan.appendDelivery(task);
+
+			// set current city
+			current = task.deliveryCity;
+		}
+		return plan;
+	}
+}
